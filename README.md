7c5f255d5068df51222974f1172f059d053404e5
maunilswadas <swadasmaunil@gmail.com>
2016-08-28 22:01:04 +0530

    Add:cs script and readme

---
+++ README.md
@@ -0,0 +1,11 @@
+<h3>How to use this plugin</h3>
+<ol>
+<li>install node dependencies by running command <i>npm install</i></li>
+<li>create a new 3d project in unity</li>
+<li>install socket.io asset in unity form <a href="https://www.assetstore.unity3d.com/en/#!/content/21721"> asset store.</a></li>
+<li>drag and drop SocketIO prefab in the scene.(SocketIO/prefabs/SocketIO.prefab)</li>
+<li>replace port number to 3000 in url property of socketIO component script </li>
+<li>create an empty component, name it 'controller' and add controller.cs script</li>
+<li>run node index.js in cmd</li>
+<li>run unity game</li>
+</ol>
---
+++ controller.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using SocketIO;
+
+
+public class controller : MonoBehaviour {
+	
+	private GameObject player;
+	string gamedata="";
+	void Start () {
+		GameObject go = GameObject.Find("SocketIO");
+		player = GameObject.Find ("player");
+		SocketIOComponent socket= go.GetComponent<SocketIOComponent>();
+		socket.On ("datarec",(SocketIOEvent obj) => {
+			//Debug.Log(obj.data);
+			Debug.Log(obj.data.ToString().Substring(9,3));
+			gamedata=obj.data.ToString().Substring(9,3);
+			player.transform.Rotate (0,float.Parse(gamedata), 0);
+		});
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnGUI() {
+		if (GUI.Button (new Rect (10, 70, 50, 30), "Click")) {
+			Debug.Log ("Clicked the button with text");
+			GameObject go = GameObject.Find("SocketIO");
+			SocketIOComponent socket= go.GetComponent<SocketIOComponent>();
+			//socket.Emit ("disconnect");
+		}
+
+	}
+
+}
