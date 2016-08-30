491e0a295a4a653e6d0156ee0b8f86402e5df296
maunilswadas <swadasmaunil@gmail.com>
2016-08-30 20:05:17 +0530

    update:keypress added

--- index.js
+++ index.js
@@ -3,40 +3,38 @@ var app =express();
 var http = require('http');
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
+var keypress = require('keypress');
+keypress(process.stdin);
 
 var data = {
 	"data":"0"
 	};
-var data2={
-	"name":"Sasuke",
-	"data":"1,1,1"
-	};;
 io.on("connection",function(socket){
 	socket.on("disconnect",function(){
 		//flag=false;
 		data.data=0;
 		console.log("user disconneted");
 	});
-	for(var i=0; i<50;i++){
-		data.data=Number(data.data)+3+"";
+	/*for(var i=0; i<50;i++){
+		setTimeout(function(){
+			data.data=Number(data.data)+3+"";
 		if(Number(data.data)<10)
 			data.data="00"+data.data;
 		else if(Number(data.data)<100)
 			data.data="0"+data.data;
-		socket.emit("datarec",data);
-		//socket.emit("datarec",data2);
-	}
-	console.log('a user connected');
-	/*var flag=1;
-	function sendData(data){
-		while(flag==1){
-			var newdata = data;//add mpu6050 data here
-			setTimeout(function(){cosole.log("datasent");},600);
+			socket.emit("datarec",data);
+		},0);*/
+		
+	process.stdin.on('keypress', function (ch, key) {
+	console.log('got "keypress"', key.name);
+		if (key && key.ctrl && key.name == 'c') {
+			process.stdin.pause();
 		}
-	}
-	if(flag==1)
-		sendData();
-	*/
+	socket.emit("datarec",key);
+	});
+	process.stdin.setRawMode(true);
+	process.stdin.resume();
+	console.log('a user connected');
 });
 server.listen(3000,function(){
 	console.log("server is running");
