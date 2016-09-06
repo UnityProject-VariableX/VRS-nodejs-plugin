cb8559f8a3d9f3327163abd91b39ce0234a41747
maunil <swadasmaunil@gmail.com>
2016-09-06 13:24:00 +0000

    reading data from mpu6050 and sending it to unity

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ var address = 0x68;
 var i2c1 = i2c.openSync(1);
 var sensor = new MPU6050(i2c1, address);
 
+var datasend={
+	"rot":""
+};
+
 io.on("connection",function(socket){
 	socket.on("disconnect",function(){
 		console.log("user disconneted");
@@ -20,42 +24,12 @@ io.on("connection",function(socket){
 	console.log('a user connected');
 	var inte = setInterval(function(){
 	var data = sensor.readSync();
-	socket.emit("datarec",data);
-	console.log(data);
+	datasend.rot=data.rotation.z;
+	socket.emit("datarec",datasend);
+	console.log(datasend);
 	},100);
 
 });
 server.listen(3000,function(){
 	console.log("server is running");
 });
-
-
-
-
-
-//kachro
-//var keypress = require('keypress');
-//keypress(process.stdin);
-//var data = {
-//	"data":"0"
-//	};
-/*for(var i=0; i<50;i++){
-		setTimeout(function(){
-			data.data=Number(data.data)+3+"";
-		if(Number(data.data)<10)
-			data.data="00"+data.data;
-		else if(Number(data.data)<100)
-			data.data="0"+data.data;
-			socket.emit("datarec",data);
-		},0);
-		
-	//process.stdin.on('keypress', function (ch, key) {
-	//console.log('got "keypress"', key.name);
-		//if (key && key.ctrl && key.name == 'c') {
-		//	process.stdin.pause();
-	//	}
-	//socket.emit("datarec",key);
-	//});
-	//process.stdin.setRawMode(true);
-	//process.stdin.resume();
-	*/
